ff2008f60f125fa37d578a12d84cc9ae770edbb0
George Kosmidis <georgekosmidis@outlook.com>
2020-03-23 15:45:14 +0100

    Benchmarking for comparison between the native implementation and the HttpClientService approach.

---
+++ Benchmark.md
@@ -0,0 +1,8 @@
+# Benchmarking
+
+This is a simple comparison of the native implementation (with IdentityModel) vs the HttpClientService implementation.
+
+![Benchmarking](https://github.com/georgekosmidis/IdentityServer4.Contrib.HttpClientService/tree/master/benchmark/benchmark_v2.3.0.png)
+*The HttpClientService is **70% to 80% faster** because of the caching that implements.*
+
+Check and download the [bechmarking tool]().
---
+++ benchmark/IdentityServer4.Contrib.HttpClientService.Benchmark/Implementations/NativeImplementation.cs
@@ -0,0 +1,34 @@
+﻿using IdentityModel;
+using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace IdentityServer4.Contrib.HttpClientService.Benchmark.Implementations
+{
+    public static class NativeImplementation
+    {
+        public static async Task Sample(HttpClient httpClient)
+        {
+
+            var identityServerResponse = await httpClient.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = "https://demo.identityserver.io/connect/token",
+                ClientId = "m2m",
+                ClientSecret = "secret",
+                Scope = "api",
+            });
+
+            if (identityServerResponse.IsError)
+                throw new InvalidOperationException(identityServerResponse.Error);
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", identityServerResponse.AccessToken);
+            var apiResponse = await httpClient.GetAsync("https://demo.identityserver.io/api/test");
+
+            if (!apiResponse.IsSuccessStatusCode)
+                throw new InvalidOperationException(identityServerResponse.Error);
+
+        }
+    }
+}
